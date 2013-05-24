988df74a8d3fcd9a79ad498a83e619b1bf98758d
Rubtsov Michael <rubtsov.michael@gmail.com>
2013-05-24 17:52:09 +0300

    converter

--- src/main/java/com/rubtsov/EntryConverter.java
+++ src/main/java/com/rubtsov/EntryConverter.java
@@ -5,23 +5,35 @@ import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.beanvalidation.SpringConstraintValidatorFactory;
 
 import com.rubtsov.entry.service.EntriesServise;
 
-@FacesConverter("entryConverter")
+@Component(value = "entryConverter")
 public class EntryConverter implements Converter {
+
+    private static final Logger LOG = Logger.getLogger(EntryConverter.class);
+    
+    public EntryConverter() {
+        LOG.debug(this.getClass().getCanonicalName() + " instantinated.");
+    }
 
     @Autowired
     private EntriesServise entriesServise;
     
     @Override
     public Object getAsObject(FacesContext context, UIComponent component, String value) {
+        LOG.debug("getAsObject " +  value);
         return entriesServise.getEntryByName(value);
     }
 
     @Override
     public String getAsString(FacesContext context, UIComponent component, Object value) {
+        LOG.debug("getAsString " +  value);
         return value.toString();
     }
 
